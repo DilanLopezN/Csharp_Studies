bce75aac7331d4cd77120a6d5b2703b088ffa327
Dilan Lopez <dilanlopez009@gmail.com>
2022-12-20 19:20:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Fundamentos_C_.models;
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Dilan";
+pessoa1.Idade = 22;
+pessoa1.Apresentar();
+
+Livraria livraria1 = new Livraria();
+livraria1.InStock = 0;
+livraria1.Book = "Clean Code";
+livraria1.BuyBook();
